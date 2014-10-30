3561eb4ed042c021d70f481260b5df1bbbc82a83
EJohannsen <ejohannsen7@gmail.com>
2014-10-30 11:12:49 -0500

    added some images and also some directions

--- testing/map.js
+++ testing/map.js
@@ -1,21 +1,22 @@
 function initialize() {
 	var mapCanvas = document.getElementById('map_canvas');
 	var mapOptions = {
-	  center: new google.maps.LatLng(47.4736, -94.9903),
+	  center: new google.maps.LatLng(47.475237, -94.880512),
 	  zoom: 11,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
 	map = new google.maps.Map(mapCanvas, mapOptions);
 	markers = [];
 	allMarkers = createAllMarkers();
-	
+	dirButton = document.getElementById('directions');
+	dirButton.disabled = true;
 	//kat's book nook llc
 	//book world
 	//roy's comics and games
 	//bill's used furniture and appliances
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-function setMarker(latLon, name)
+function setMarker(latLon, name, addr, phone, eml, img)
 {
     var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
     var marker = new google.maps.Marker({
@@ -32,15 +33,19 @@ function setMarker(latLon, name)
 	    header = document.createElement('h2');
 	    header.innerHTML = name;
 	    someDiv.appendChild(header);
-	    contact = document.createElement('p');
-	    address = "2432 Adams Ave" + "<br>";
-	    tele = "(218) 444-1234" + "<br>";
-	    email = "<a href='" + "something@domain.com" + "'>" + "something@domain.com" + "</a>";
+	    var contact = document.createElement('p');
+	    var address = addr + "<br>";
+	    var tele = phone + "<br>";
+	    var email = "<a href='" + eml + "'>" + eml + "</a>";
 	    contact.innerHTML = address + tele + email;
-	    website = document.createElement('p');
-	    website.innerHTML = "<a href='" + "http://goodwill.org" + "'>" + "goodwill.org" + "</a>";
+	    var image = document.createElement('p');
+	    image.innerHTML = "<img src=" + img + ">"; //might need height and width and alt
+	    //var website = document.createElement('p');
+	    //website.innerHTML = "<a href='" + "http://goodwill.org" + "'>" + "goodwill.org" + "</a>";
 	    someDiv.appendChild(contact);
-	    someDiv.appendChild(website);
+	    someDiv.appendChild(image);
+	    dirButton.disabled = false;
+	    currentLatLon = latLon;
     });
     markers.push(marker);
 }
@@ -56,14 +61,52 @@ function clearMarkers()
 function getData(item)
 {
 	clearMarkers();
-	//setMarker(new google.maps.LatLng(47.472173, -94.881648), 'Goodwill');
+	map.setZoom(11);
+	map.setCenter(new google.maps.LatLng(47.475237, -94.880512));
 	//connect to database and get all stores with item
+	
+	//could always try to connect to database and if there is no connection just call a
+	//different function that uses the static data
 	for (var i = 0; i < allMarkers.length; i++)
 	{
 		if (allMarkers[i][7] == item)
 		{
-			setMarker(new google.maps.LatLng(allMarkers[i][1], allMarkers[i][2]), allMarkers[i][0]);
+			setMarker(new google.maps.LatLng(allMarkers[i][1], allMarkers[i][2]), allMarkers[i][0], allMarkers[i][3], allMarkers[i][4], allMarkers[i][5], allMarkers[i][6]);
 		}
+	}
+}
+
+function getDirections()
+{
+	if ("geolocation" in navigator)
+	{
+		clearMarkers();
+		navigator.geolocation.getCurrentPosition(function(position)
+		{
+			var from = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			var to = currentLatLon;
+			var directionsService = new google.maps.DirectionsService();
+			var directionsRequest =
+			{
+				origin: from,
+				destination: to,
+				travelMode: google.maps.TravelMode.DRIVING
+			};
+			directionsService.route(directionsRequest, function(response, status)
+			{
+				if (status == google.maps.DirectionsStatus.OK)
+				{
+					new google.maps.DirectionsRenderer({
+						map: map,
+						directions: response
+					});
+				}
+			});
+		});
+	}
+	else
+	{
+		alert("Location finder not available");
 	}
 }
 
@@ -74,80 +117,80 @@ function createAllMarkers()
 	goodwill.push("Goodwill");
 	goodwill.push("47.472173");
 	goodwill.push("-94.881648");
-	goodwill.push("Address");
-	goodwill.push("Phone");
+	goodwill.push("407 Beltrami Ave NW, Bemidji, MN 56601");
+	goodwill.push("(218) 759-2147");
 	goodwill.push("Email");
-	goodwill.push("goodwill.png");
+	goodwill.push("goodwill.jpeg");
 	goodwill.push("clothing");
 	allStores.push(goodwill);
 	var tkz = [];
 	tkz.push("Tk'z Closet");
 	tkz.push("47.470792");
 	tkz.push("-94.882243");
-	tkz.push("Address");
-	tkz.push("Phone");
+	tkz.push("217 3rd St NW, Bemidji, MN 56601");
+	tkz.push("(218) 444-8406");
 	tkz.push("Email");
-	tkz.push("tkz.png");
+	tkz.push("tkz.jpeg");
 	tkz.push("clothing");
 	allStores.push(tkz);
 	var twice = [];
 	twice.push("Twice but Nice");
 	twice.push("47.471883");
 	twice.push("-94.881031");
-	twice.push("Address");
-	twice.push("Phone");
+	twice.push("411 Beltrami Ave NW, Bemidji, MN 56601");
+	twice.push("(218) 751-4241");
 	twice.push("Email");
-	twice.push("twice.png");
+	twice.push("twice.jpeg");
 	twice.push("clothing");
 	allStores.push(twice);
 	var mbfc = [];
 	mbfc.push("My Best Friend's Closet");
 	mbfc.push("47.469740");
 	mbfc.push("-94.881187");
-	mbfc.push("Address");
-	mbfc.push("Phone");
+	mbfc.push("209 Beltrami Ave NW, Bemidji, MN 56601");
+	mbfc.push("(218) 308-2305");
 	mbfc.push("Email");
-	mbfc.push("mbfc.png");
+	mbfc.push("mbfc.jpeg");
 	mbfc.push("clothing");
 	allStores.push(mbfc);
 	var phils = [];
 	phils.push("St. Philip's Clothing Depot");
 	phils.push("47.475237");
 	phils.push("-94.880512");
-	phils.push("Address");
-	phils.push("Phone");
+	phils.push("720 Beltrami Ave NW, Bemidji, MN 56601");
+	phils.push("(218) 444-3835");
 	phils.push("Email");
-	phils.push("phils.png");
+	phils.push("phils.jpeg");
 	phils.push("clothing");
 	allStores.push(phils);
 	var tnt = [];
 	tnt.push("T-N-T Secondhand");
 	tnt.push("47.483852");
 	tnt.push("-94.927327");
-	tnt.push("Address");
-	tnt.push("Phone");
+	tnt.push("1408 Adams Ave NW, Bemidji, MN 56601");
+	tnt.push("(218) 751-2103");
 	tnt.push("Email");
-	tnt.push("tnt.png");
+	tnt.push("tnt.jpeg");
 	tnt.push("clothing");
 	allStores.push(tnt);
 	var aunt = [];
-	aunt.push("Aunt Ellie's");
+	aunt.push("Aunt Ellie's Attic");
 	aunt.push("47.453613");
 	aunt.push("-94.863358");
-	aunt.push("Address");
-	aunt.push("Phone");
+	aunt.push("824 Washington Ave S, Bemidji, MN 56601");
+	aunt.push("(218) 444-4334");
 	aunt.push("Email");
-	aunt.push("aunt.png");
+	aunt.push("aunt.jpeg");
 	aunt.push("clothing");
 	allStores.push(aunt);
 	var bensons = [];
 	bensons.push("Benson's Auto");
-	bensons.push("47.453113");
-	bensons.push("-94.863958");
-	bensons.push("Address");
-	bensons.push("Phone");
+	bensons.push("47.473653");
+	bensons.push("-94.919421");
+	bensons.push("6941 Fairgrounds Rd NW , Bemidji, MN 56601");
+	bensons.push("(218) 444-4414");
 	bensons.push("Email");
-	bensons.push("bensons.png");
+	bensons.push("bensons.jpeg");
 	bensons.push("cars");
 	allStores.push(bensons);
 	return allStores;
